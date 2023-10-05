d316404183d23a21614e54728d1e7172d1982ba1
naisiae.kasura@gmail.com <imani.kasura@student.moringaschool.com>
2023-10-05 07:58:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+var num1 = 31;
+var num2 = 2;
 
+var multiply = num1 * num2;
+
+console.log(multiply);
+
+var random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random);
+
+var num3 = 18;
+var num4 = 7;
+
+var mod = num3 % num4;
+
+console.log(mod);
+
+var setOfNumbers = [5, 12, 20, 8, 15, 3];
+var max = Math.max(...setOfNumbers);
+
+console.log(max); 
 // Write your code here
